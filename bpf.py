39d954c28f6fe104d72d1f95c934e2231376e28b
Daniel Roethlisberger <daniel@roe.ch>
2025-07-22 23:12:10 +0200

    Add BPFCompiler for testing

--- bpf.py
+++ bpf.py
@@ -215,6 +215,91 @@ BPF_COPX        = 0x40 # NetBSD
 BPF_TXA         = 0x80
 
 
+class BPFCompiler:
+    """
+    BPF compiler to assist in testing.  Unused in the plugin.
+
+    Designed to make conversion from the C form as direct as possible.
+    """
+    def __init__(self, endianness=binja.Endianness.LittleEndian):
+        if endianness == binja.Endianness.LittleEndian:
+            self._insn_layout = '<HBBI'
+        elif endianness == binja.Endianness.BigEndian:
+            self._insn_layout = '>HBBI'
+        self._insns = []
+
+    def _insn(self, code, jt, jf, k):
+        self._insns.append(struct.pack(self._insn_layout, code, jt, jf, k))
+
+    def BPF_STMT(self, opcode, operand):
+        self._insn(opcode, 0, 0, operand)
+
+    def BPF_JUMP(self, opcode, operand, true_offset, false_offset):
+        self._insn(opcode, true_offset, false_offset, operand)
+
+    def __bytes__(self):
+        return b''.join(self._insns)
+
+    @classmethod
+    def make_examples(cls, path, endianness=binja.Endianness.LittleEndian):
+        """
+        Writes the classic BPF filter examples given in the BSD manual pages to
+        path.
+        """
+        ETHERTYPE_REVARP = 0x8035
+        REVARP_REQUEST = 3
+        SIZEOF_ETHER_ARP = 28
+        SIZEOF_ETHER_HEADER = 14
+        c = cls(endianness)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 12)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, ETHERTYPE_REVARP, 0, 3)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 20)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, REVARP_REQUEST, 0, 1)
+        c.BPF_STMT(BPF_RET+BPF_K, SIZEOF_ETHER_ARP + SIZEOF_ETHER_HEADER)
+        c.BPF_STMT(BPF_RET+BPF_K, 0)
+        with open(f"{path}/example_rarp.bpfcode", "wb") as f:
+            f.write(bytes(c))
+
+        ETHERTYPE_IP = 0x0800
+        c = cls(endianness)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 12)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, ETHERTYPE_IP, 0, 8)
+        c.BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 26)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x8003700f, 0, 2)
+        c.BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 30)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x80037023, 3, 4)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x80037023, 0, 3)
+        c.BPF_STMT(BPF_LD+BPF_W+BPF_ABS, 30)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 0x8003700f, 0, 1)
+        c.BPF_STMT(BPF_RET+BPF_K, 0xFFFFFFFF)
+        c.BPF_STMT(BPF_RET+BPF_K, 0)
+        with open(f"{path}/example_ipaddr.bpfcode", "wb") as f:
+            f.write(bytes(c))
+
+        ETHERTYPE_IP = 0x0800
+        IPPROTO_TCP = 6
+        c = cls(endianness)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 12)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, ETHERTYPE_IP, 0, 10)
+        c.BPF_STMT(BPF_LD+BPF_B+BPF_ABS, 23)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, IPPROTO_TCP, 0, 8)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_ABS, 20)
+        c.BPF_JUMP(BPF_JMP+BPF_JSET+BPF_K, 0x1fff, 6, 0)
+        c.BPF_STMT(BPF_LDX+BPF_B+BPF_MSH, 14)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_IND, 14)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 79, 2, 0)
+        c.BPF_STMT(BPF_LD+BPF_H+BPF_IND, 16)
+        c.BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, 79, 0, 1)
+        c.BPF_STMT(BPF_RET+BPF_K, 0xFFFFFFFF)
+        c.BPF_STMT(BPF_RET+BPF_K, 0)
+        with open(f"{path}/example_tcpfinger.bpfcode", "wb") as f:
+            f.write(bytes(c))
+
+
+#BPFCompiler.make_examples("/tmp", binja.Endianness.LittleEndian)
+#BPFCompiler.make_examples("/tmp", binja.Endianness.BigEndian)
+
+
 class BPFInstruction:
     class InvalidStorageError(Exception):
         pass
